0e92a314e970afbbaa6823d2c8a3c9da84f03055
Cen-2-rion <atrohov1982@gmail.com>
2024-07-19 02:58:25 +0300

    editing

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,8 +2,8 @@ import getBuffer from '../ArrayBuffer';
 import ArrayBufferConverter from '../ArrayBufferConverter';
 
 test('loading data and converting ArrayBuffer to string', () => {
-    const converter = new ArrayBufferConverter();
-    converter.load(getBuffer()); // подгружаем буффер в конвертер
+  const converter = new ArrayBufferConverter();
+  converter.load(getBuffer()); // подгружаем буффер в конвертер
 
-    expect(converter.toString()).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
+  expect(converter.toString()).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
 });
